30277e7cb205cd2ad985e6d2a16314b6350baf77
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-01-27 23:34:50 -0500

    Remove unused classes and imports

--- src/org/usfirst/frc/team1076/robot/Robot.java
+++ src/org/usfirst/frc/team1076/robot/Robot.java
@@ -6,17 +6,10 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
-import java.net.SocketException;
-
 import org.strongback.Strongback;
-
-import com.ctre.CANTalon;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-import org.usfirst.frc.team1076.robot.commands.ExampleCommand;
-import org.usfirst.frc.team1076.robot.subsystems.ExampleSubsystem;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -26,10 +19,6 @@ import org.usfirst.frc.team1076.robot.subsystems.ExampleSubsystem;
  * directory.
  */
 public class Robot extends IterativeRobot {
-
-	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	public static OI oi;
-
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
@@ -39,10 +28,7 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void robotInit() {
-		oi = new OI();
-		chooser.addDefault("Default Auto", new ExampleCommand());
-		// chooser.addObject("My Auto", new MyAutoCommand());
-		SmartDashboard.putData("Auto mode", chooser);
+	    Strongback.start();
 	}
 
 	/**
