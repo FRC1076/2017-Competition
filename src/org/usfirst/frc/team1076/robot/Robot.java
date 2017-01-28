7c01b8ab19c49a1f82ffacc794689dc97a20f8e9
Unknown <sahmed2026@gmail.com>
2017-01-28 17:10:26 -0500

    Fixed Compilation Errors

--- src/org/usfirst/frc/team1076/robot/Robot.java
+++ src/org/usfirst/frc/team1076/robot/Robot.java
@@ -6,16 +6,16 @@ import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
+import java.net.SocketException;
+
 import org.strongback.Strongback;
 import org.strongback.components.Motor;
 import org.strongback.hardware.Hardware;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
-import org.usfirst.frc.team1076.robot.commands.ExampleCommand;
 import org.usfirst.frc.team1076.robot.commands.TeleopCommand;
 import org.usfirst.frc.team1076.robot.subsystems.Drivetrain;
-import org.usfirst.frc.team1076.robot.subsystems.ExampleSubsystem;
 import org.usfirst.frc.team1076.robot.vision.VisionReceiver;
 
 /**
@@ -26,39 +26,38 @@ import org.usfirst.frc.team1076.robot.vision.VisionReceiver;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
-	public static OI oi;
 	Gamepad gamepad = new Gamepad(0);
 	Command autonomousCommand;
-	Motor left = Hardware.Motors.talonSRX(0).invert(); // This motor is placed backwards on the robot
+	Motor left = Hardware.Motors.talonSRX(0).invert(); // This motor is placed
+														// backwards on the
+														// robot
 	Motor right = Hardware.Motors.talonSRX(1);
 	Drivetrain drivetrain = new Drivetrain(left, right);
 	TeleopCommand teleopCommand = new TeleopCommand(gamepad, drivetrain);
 	SendableChooser<Command> chooser = new SendableChooser<>();
 
 	VisionReceiver receiver;
-    public static final String IP = "0.0.0.0"; //"10.10.76.2"; 
-    public static final int VISION_PORT = 5880;
-    
+	public static final String IP = "0.0.0.0"; // "10.10.76.2";
+	public static final int VISION_PORT = 5880;
+
 	/**
 	 * This function is run when the robot is first started up and should be
 	 * used for any initialization code.
 	 */
 	@Override
 	public void robotInit() {
-	    Strongback.start();
-      gamepad = new Gamepad(0);
-      SmarterDashboard.putDefaultNumber("Left Factor", 1);
-    SmarterDashboard.putDefaultNumber("Right Factor", 1);
+		Strongback.start();
+		gamepad = new Gamepad(0);
+		SmarterDashboard.putDefaultNumber("Left Factor", 1);
+		SmarterDashboard.putDefaultNumber("Right Factor", 1);
 
-		chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmarterDashboard.putDefaultNumber("Show Vision", 1);
-        try {
-            receiver = new VisionReceiver(IP, VISION_PORT);
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
+		try {
+			receiver = new VisionReceiver(IP, VISION_PORT);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
 		SmarterDashboard.putData("Auto mode", chooser);
 	}
 
@@ -69,22 +68,23 @@ public class Robot extends IterativeRobot {
 	 */
 	@Override
 	public void disabledInit() {
-        drivetrain.leftFactor = SmarterDashboard.getNumber("Left Factor", 1);
-        drivetrain.rightFactor = SmarterDashboard.getNumber("Right Factor", 1);
+		drivetrain.leftFactor = SmarterDashboard.getNumber("Left Factor", 1);
+		drivetrain.rightFactor = SmarterDashboard.getNumber("Right Factor", 1);
 	}
- 
-int debugCount = 0;
-@Override
-public void disabledPeriodic() {
+
+	int debugCount = 0;
+
+	@Override
+	public void disabledPeriodic() {
 		Scheduler.getInstance().run();
 		if (debugCount++ % 100 == 0 && SmarterDashboard.getNumber("Show Vision", 0) == 1) {
-		    if (receiver == null) {
-		        Strongback.logger().warn("VisionReceiver is null on IP " + IP + " and port number " + VISION_PORT);
-		    } else {
-	            receiver.receive();
-		        Strongback.logger().info(receiver.getData().toString());
-		    }
-		    
+			if (receiver == null) {
+				Strongback.logger().warn("VisionReceiver is null on IP " + IP + " and port number " + VISION_PORT);
+			} else {
+				receiver.receive();
+				Strongback.logger().info(receiver.getData().toString());
+			}
+
 		}
 	}
 
@@ -126,25 +126,26 @@ public void disabledPeriodic() {
 	@Override
 	public void teleopInit() {
 		// This makes sure that the autonomous stops running when
-        // teleop starts running. If you want the autonomous to 
-        // continue until interrupted by another command, remove
-        // this line or comment it out.
-    	Strongback.logger().info("I LIVE!");
-    	Strongback.submit(teleopCommand);
-        if (autonomousCommand != null) autonomousCommand.cancel();
-    }
+		// teleop starts running. If you want the autonomous to
+		// continue until interrupted by another command, remove
+		// this line or comment it out.
+		Strongback.logger().info("I LIVE!");
+		Strongback.submit(teleopCommand);
+		if (autonomousCommand != null)
+			autonomousCommand.cancel();
+	}
 
-    /**
-     * This function is called periodically during operator control
-     */
-    public void teleopPeriodic() {
-        Scheduler.getInstance().run();
-    }
-    
-    /**
-     * This function is called periodically during test mode
-     */
-    public void testPeriodic() {
-        LiveWindow.run();
-    }
+	/**
+	 * This function is called periodically during operator control
+	 */
+	public void teleopPeriodic() {
+		Scheduler.getInstance().run();
+	}
+
+	/**
+	 * This function is called periodically during test mode
+	 */
+	public void testPeriodic() {
+		LiveWindow.run();
+	}
 }
