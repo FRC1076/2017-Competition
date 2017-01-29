148bbffc512de2d5a0f94fe1e99b6e0dfcac4d3c
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-01-28 21:01:01 -0500

    Add documentation

--- src/org/usfirst/frc/team1076/robot/commands/TurnWithGyro.java
+++ src/org/usfirst/frc/team1076/robot/commands/TurnWithGyro.java
@@ -4,12 +4,25 @@ import org.strongback.command.Command;
 import org.strongback.components.Gyroscope;
 import org.usfirst.frc.team1076.robot.subsystems.Drivetrain;
 
+/**
+ * TurnWithGyro takes a Strongback Gyroscope, a Drivetrain,
+ * a speed, and a target angle in degrees and attempts to turn
+ * to that angle as specified by the gyro. Note that positive angles turn *right*
+ * and negative angles turn *left*.
+ */
 public class TurnWithGyro extends Command {
     Gyroscope gyro;
     Drivetrain drivetrain;
     double speed;
     double targetAngle;
     
+    /**
+     * Create a new TurnWithGyro 
+     * @param gyro        a Strongback Gyroscope
+     * @param drivetrain  a drivetrain
+     * @param speed       speed from -1 to 1 (inclusive) to drive at
+     * @param targetAngle angle, in degrees, to turn. Note that positive is right, while negative is left
+     */
     public TurnWithGyro(Gyroscope gyro, Drivetrain drivetrain, double speed, double targetAngle) {
         super(drivetrain);
         this.gyro = gyro;
