b1d4effbbe2779142cb65181e25d78ec64376cea
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-02-18 18:24:36 -0500

    Create Winch
    The winch allows the robot to climb at the end of a match. It can extend and retract.

---
+++ src/org/usfirst/frc/team1076/robot/subsystems/Winch.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team1076.robot.subsystems;
+
+import org.strongback.command.Requirable;
+import org.strongback.components.Motor;
+
+public class Winch implements Requirable {
+    Motor winch;
+
+    /**
+     * Create a winch from one motor
+     * @param winch     the motor to use for the winch
+     */
+    public Winch(Motor winch) {
+        this.winch = winch;
+    }
+    
+    /**
+     * Create a winch from two motors composed to work in unison
+     * @param motor1    a motor for the winch
+     * @param motor2    a motor for the winch
+     */
+    public Winch(Motor motor1, Motor motor2) {
+        this(Motor.compose(motor1, motor2));
+    }
+    
+    /**
+     * Set the speed of the winch. Positive extends, negative retracts
+     */
+    public void setSpeed(double speed) {
+        winch.setSpeed(speed);
+    }
+    
+    /**
+     * Extend the winch at the set speed
+     * @param speed
+     */
+    public void extend(double speed) {
+        winch.setSpeed(speed);
+    }
+    
+    /**
+     * Retract the winch at the set speed
+     * @param speed
+     */
+    public void retract(double speed) {
+        winch.setSpeed(-speed);
+    }
+}
---
+++ testsrc/org/usfirst/frc/team1076/test/TestWinch.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team1076.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.strongback.components.Motor;
+import org.strongback.mock.Mock;
+import org.usfirst.frc.team1076.robot.subsystems.Winch;
+
+public class TestWinch {
+    public static final double EPSILON = 1E-10;
+    Motor motor = Mock.stoppedMotor();
+    Winch winch = new Winch(motor);
+    
+    @Before
+    public void reset() {
+        motor.setSpeed(0);
+    }
+
+    @Test
+    public void testSetSpeed() {
+        winch.extend(1.0);
+        assertEquals("Winch should be set to the right speed", 1.0, motor.getSpeed(), EPSILON);
+        winch.extend(-1.0);
+        assertEquals("Winch should be set to the right speed", -1.0, motor.getSpeed(), EPSILON);
+    }
+
+    
+    @Test
+    public void testExtend() {
+        winch.extend(1.0);
+        assertEquals("Winch should extend", 1.0, motor.getSpeed(), EPSILON);
+    }
+
+    @Test
+    public void testRetract() {
+        winch.retract(1.0);
+        assertEquals("Winch should retract", -1.0, motor.getSpeed(), EPSILON);
+    }
+
+}
