1ecd2c20ad73be5873e741024fa3b1d850480fcc
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-02-19 18:18:55 -0500

    Create Brake command

---
+++ src/org/usfirst/frc/team1076/robot/commands/BrakeCommand.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team1076.robot.commands;
+
+import org.strongback.Strongback;
+import org.strongback.command.Command;
+import org.strongback.components.Solenoid;
+import org.usfirst.frc.team1076.robot.Gamepad;
+import org.usfirst.frc.team1076.robot.Gamepad.GamepadAxis;
+
+public class BrakeCommand extends Command {
+    Solenoid brake;
+    Gamepad gamepad;
+    
+    public BrakeCommand(Solenoid brake, Gamepad gamepad) {
+        this.brake = brake;
+        this.gamepad = gamepad;
+    }
+    
+    public boolean execute() {
+        final double trigger = gamepad.getAxis(GamepadAxis.RightTrigger);
+        if (trigger > 0.5) {
+            brake.extend();
+        } else {
+            brake.retract();
+        }
+        return false;
+    }
+}
