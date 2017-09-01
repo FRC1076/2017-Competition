62def1d602a06a35ddd5dfe2bd0fee547a7ef9f3
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-09-01 14:41:35 -0400

    Create barebones robot.py
    It's the hello world of robotpy!

---
+++ src/robot.py
@@ -0,0 +1,23 @@
+import wpilib
+from commandbased import CommandBasedRobot
+
+
+class MemeBot(CommandBasedRobot):
+    def robotInit(self):
+        pass
+
+    def autonomousInit(self):
+        pass
+
+    def autonomousPerodic(self):
+        pass
+
+    def teleopInit(self):
+        pass
+
+    def teleopPerodic(self):
+        pass
+
+
+if __name__ == '__main__':
+    wpilib.run(MemeBot)
