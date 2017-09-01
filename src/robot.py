549dbca2bff8510e326d5afaa9d638091a18e0c5
Aaron Kofsky <a2aarontothe2@gmail.com>
2017-09-01 17:52:08 -0400

    Add some basic motor and gamepads to `robotInit`

--- src/robot.py
+++ src/robot.py
@@ -1,10 +1,18 @@
 import wpilib
-from commandbased import CommandBasedRobot
+
+LEFT_HAND = 0
+RIGHT_HAND = 1
 
 
-class MemeBot(CommandBasedRobot):
+class MemeBot(wpilib.IterativeRobot):
+
     def robotInit(self):
-        pass
+        self.left1 = wpilib.TalonSRX(3)
+        self.left2 = wpilib.TalonSRX(4)
+        self.right1 = wpilib.TalonSRX(1)
+        self.right2 = wpilib.TalonSRX(2)
+        self.driver = wpilib.XboxController(0)
+        self.operator = wpilib.XboxController(1)
 
     def autonomousInit(self):
         pass
